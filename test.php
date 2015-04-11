02c231902f0b1fc68fb3ef1e61ef4a88d84f9d15
Alex Bowers <bowersbros@gmail.com>
2015-04-12 00:40:28 +0100

    Improved version
    Thanks to Eshunt for the code.

--- test.php
+++ test.php
@@ -13,72 +13,91 @@ Level 0 Child 2
             Level 1 Child 2 Grandchild 2 Great-grandchild 1
 ';
 
-function helper($list, $indentation = '    ') {
-    $result = [];
-    $path = [];
+    function helper($list, $indentation = '    ')
+    {
+        $result = [];
+        $path = [];
 
-    foreach (explode("\n", $list) as $line) {
-        // get depth and label
-        $depth = 0;
-        while (substr($line, 0, strlen($indentation)) === $indentation) {
-            $depth += 1;
-            $line = substr($line, strlen($indentation));
-        }
+        foreach (explode("\n", $list) as $line) {
+            // get depth and label
+            $depth = 0;
+            while (substr($line, 0, strlen($indentation)) === $indentation) {
+                $depth += 1;
+                $line = substr($line, strlen($indentation));
+            }
 
-        // truncate path if needed
-        while ($depth < sizeof($path)) {
-            array_pop($path);
-        }
+            // truncate path if needed
+            while ($depth < sizeof($path)) {
+                array_pop($path);
+            }
 
-        // keep label (at depth)
-        $trimmed_line = trim($line);
-        if(!empty($trimmed_line)) {
-            $path[$depth] = $trimmed_line;
+            // keep label (at depth)
+            $trimmed_line = trim($line);
+            if (!empty($trimmed_line)) {
+                $path[$depth] = $trimmed_line;
 
-            // traverse path and add label to result
-            $parent =& $result;
-            foreach ($path as $depth => $key) {
-                if (!isset($parent[$key]) && !empty($key)) {
-                    $parent[$key] = [];
-                    break;
+                // traverse path and add label to result
+                $parent =& $result;
+                foreach ($path as $depth => $key) {
+                    if (!isset($parent[$key]) && !empty($key)) {
+                        $parent[$key] = [];
+                        break;
+                    }
+
+                    $parent =& $parent[$key];
                 }
-
-                $parent =& $parent[$key];
             }
         }
+
+        // return
+        return $result;
     }
 
-    // return
-    return $result;
-}
+    echo '<pre>';
+    $tree = helper($list);
+    $arrLines = [array_depth($tree)];
+    echo '<pre>' . makeList($tree) . '</pre>';
 
-echo '<pre>';
-$tree = helper($list);
-//print_r($tree);
-echo makeList($tree);
-//print_r(output($string));
+    function array_depth(array $array)
+    {
+        $max_depth = 1;
 
-function makeList(array $array, $level = 0, $very_first_item = true)
-{
-    $output = "\n";
+        foreach ($array as $value) {
+            if (is_array($value)) {
+                $depth = array_depth($value) + 1;
 
-    //Recursive Step: make a list with child lists
-    $count = 0;
-    foreach ($array as $key => $subArray) {
-        if ($count == count($array) - 1 && $count == 0 && $very_first_item) {
-            $prefix = '';
-            $level = -1; // Set so next level up is level 0
-        } elseif ($count == count($array) - 1) {
-            $prefix = str_repeat('|   ', $level) . '└── ';
-        } elseif ($very_first_item === true) {
-            $prefix = str_repeat('|   ', $level) . '┌── ';
-        } else {
-            $prefix = str_repeat('|   ', $level) . '├── ';
+                if ($depth > $max_depth) {
+                    $max_depth = $depth;
+                }
+            }
         }
-        $very_first_item = false;
-        $output .= $prefix . $key . makeList($subArray, $level + 1, $very_first_item);
-        $count++;
+
+        return $max_depth;
     }
 
-    return $output;
-}
+    function makeList(array $array, $level = 0, $very_first_item = true)
+    {
+        global $arrLines;
+        $output = "\n";
+        $count = 0;
+        foreach ($array as $key => $subArray) {
+            if ($count == count($array) - 1) {
+                $prefix = '└── ';
+                $arrLines[$level + 1] = '    ';
+            } elseif ($very_first_item === true) {
+                $prefix = '┌── ';
+                $arrLines[$level + 1] = '|   ';
+            } else {
+                $prefix = '├── ';
+                $arrLines[$level + 1] = '|   ';
+            }
+            $very_first_item = false;
+            for ($iCnt = 1; $iCnt <= $level; $iCnt++) {
+                $output .= $arrLines[$iCnt];
+            }
+            $output .= $prefix . $key . makeList($subArray, $level + 1, $very_first_item);
+            $count++;
+        }
+
+        return $output;
+    }
